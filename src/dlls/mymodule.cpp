f552cc1fef6219f1a5d9dbd4c1de24b980951f58
CaioRS <caiorss.rodrigues@gmail.com>
2019-03-20 00:36:48 -0300

    Update python module compilation and install instructions.

--- src/dlls/mymodule.cpp
+++ src/dlls/mymodule.cpp
@@ -56,7 +56,10 @@ PyObject* testArguments(PyObject* self, PyObject* args);
 
 auto printRange(PyObject* self, PyObject* args) -> PyObject*;
 auto taylorSeriesExp(PyObject* self, PyObject* args) -> PyObject*;
+PyObject* returnTuple(PyObject* self, PyObject* args);
+PyObject* returnDictionary(PyObject* self, PyObject* args);
 PyObject* tabulateFunction(PyObject* self, PyObject* args);
+PyObject* computeStatistics(PyObject* self, PyObject* args);
 
 static PyMethodDef ModuleFunctions [] =
 {
@@ -74,8 +77,11 @@ static PyMethodDef ModuleFunctions [] =
 	   "\n Computes exponential of a given value with taylor serie approximation."
 	   "\n Formula reference:  https://www.mathsisfun.com/algebra/taylor-series.html"
 	}
+	,{"returnTuple", &returnTuple, METH_VARARGS, nullptr}
+	,{"returnDictionary", &returnDictionary, METH_VARARGS, nullptr}
+	,{"computeStatistics", &computeStatistics, METH_VARARGS, nullptr}
 	,{"tabulateFunction", tabulateFunction, METH_VARARGS,
-	    "Tabulate some mathematical function or callable object"}
+	  "Tabulate some mathematical function or callable object"}
 	// Sentinel value used to indicate the end of function listing.
 	// All function listing must end with this value.
 	,{nullptr, nullptr, 0, nullptr}									
@@ -213,6 +219,106 @@ auto taylorSeriesExp(PyObject* self, PyObject* args) -> PyObject*
 	return Py_BuildValue("d", sum);
 }
 
+/** Function that returns multiple values as tuple object */
+PyObject* returnTuple(PyObject* self, PyObject* args)
+{
+	double x;
+	double y;
+	int    n;
+	if(!PyArg_ParseTuple(args, "ddi", &x, &y, &n))
+		return nullptr;
+	// Build tuple object containing
+	// (d: double, d: double, i: integer, s: string)
+	return Py_BuildValue("(ddis)", 2 * x + y * n, x + 2 * y - n, n, "Result");
+}
+
+
+/** Return a dictionary object */
+PyObject* returnDictionary(PyObject* self, PyObject* args)
+{
+	double x;
+	double y;
+	int    n;
+	if(!PyArg_ParseTuple(args, "ddi", &x, &y, &n))
+		return nullptr;
+	
+	// Create new dictionary object 
+	PyObject* pDict = PyDict_New();
+
+	PyObject* p = nullptr;
+	// Fill dictionary
+	p = Py_BuildValue("d", 2 * x + y * n);
+	PyDict_SetItemString(pDict, "alpha", p);
+	Py_DECREF(p);
+	
+	p = Py_BuildValue("i", 3 * n) ;
+	PyDict_SetItemString(pDict, "size", p);
+	Py_DECREF(p);
+
+	p = Py_BuildValue("s", "Some string") ;
+	PyDict_SetItemString(pDict, "beta", p);
+	Py_DECREF(p);	
+	
+	return pDict;
+}
+
+PyObject* computeStatistics(PyObject* self, PyObject* args)
+{
+
+	std::cerr << " [TRACE] " << __FILE__ << "(" << __LINE__ << ") - "
+			  << "Calling function: " << __FUNCTION__ << "\n";
+
+	std::cerr << " [TRACE] Arguments = "; PyObject_Print(args, stdout, 0); std::cerr << "\n";
+	
+	PyObject* pSeq;
+	// Parse function argument as sequence
+	if(!PyArg_ParseTuple(args, "O", &pSeq))
+		return nullptr;
+
+	pSeq = PySequence_Fast(pSeq, "Expected iterable arguments");
+	if(pSeq == nullptr) return nullptr;
+
+	int numberOfElements = PySequence_Fast_GET_SIZE(pSeq);
+	std::cerr << " [TRACE] numberOfElements = " << numberOfElements << "\n";
+
+	PyObject* pItem = nullptr;
+	double x;
+	double sum;
+	
+	for(int n = 0; n < numberOfElements; n++)
+	{
+		pItem = PySequence_Fast_GET_ITEM(pSeq, n);
+		if(!pItem) {
+			Py_DECREF(pSeq);
+			return nullptr;
+		}
+		// Print item 
+		std::cout << "item[" << n << "] = ";
+		PyObject_Print(pItem, stdout, 0);
+		std::cout << "\n";
+		
+		x = PyFloat_AsDouble(pItem);
+		if(PyErr_Occurred() != nullptr){
+			PyErr_SetString(PyExc_TypeError, "Error: expected float point.");
+			return nullptr;
+		}
+		sum += x;
+	}
+
+	double mean = sum / numberOfElements;
+	std::cout << "\n (C++) Statics Result " << "\n";
+	std::cout << "---------------" << "\n";
+	std::cout << " Sum  = " << sum << "\n";
+	std::cout << " Mean = " << mean << "\n";
+	
+	Py_DECREF(pSeq);
+	return Py_BuildValue(""); // Return None 
+}
+
+
+/** Function with callback - A callback can be any function or object
+ * with the __call__ method. 
+ */
 PyObject* tabulateFunction(PyObject* self, PyObject* args)
 {
 	PyObject* pObj = nullptr;
